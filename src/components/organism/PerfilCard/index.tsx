cd7a8cf4fdb1e686c344b6baea38cba3e416eb35
inNN-Abner <abner.faculdade440@gmail.com>
2025-03-26 01:09:35 -0300

    Alterando o "PefilCard" para receber os valores da API

--- src/components/organism/PerfilCard/index.tsx
+++ src/components/organism/PerfilCard/index.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import perfil from '~/../archives/perfil'
 import { Windows, Subcontainer, Photo, ContactText, InfoText  } from '~/components'
+import * as SecureStore from 'expo-secure-store'
+
 
 export const PerfilCard = ({ navigation }) => {
+  const [nameValue, setNameValue] = useState('')
+  const [emailValue, setEmailValue] = useState('')
+  const [teamsValue, setTeamsValue] = useState('')
+  const [userData, setUserData] = useState<{ nome: string; foto: number; curso: string } | null>(null);
+  const [selectedCourse, setSelectedCourse] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loadUser = async () => {
+    const userData = await SecureStore.getItemAsync('user')
+    
+      if (userData) {
+        const data = JSON.parse(userData)
+        setUserData(data)
+        setNameValue(data.nome || '')
+        setEmailValue(data.email || '')
+        setTeamsValue(data.teams || '')
+        setSelectedCourse(data.curso || 'Clique para selecionar')
+      }
+    }
+    loadUser()
+  }, [])
+      if (!userData) {
+        return <InfoText>Carregando...</InfoText>
+      }
     
     return (
         <Windows
@@ -12,7 +38,7 @@ export const PerfilCard = ({ navigation }) => {
             mgTop='0'
             >
             <Photo
-                source={perfil.photo}
+                idFoto = {userData.foto}
                 hgt='125'
                 wdt='80'
                 bdRd='20'
@@ -20,17 +46,17 @@ export const PerfilCard = ({ navigation }) => {
             
             <Subcontainer mgLeft='5' mgTop='0' hgt='125' wdt='250' align='left' justify='center' bg='white'>
 
-                <ContactText color='whiteGreen' fontSize='12' mgTop='-5'>Nome do professor(a):</ContactText>
-                <InfoText color='whiteGreen' children={perfil.name} fontWgt='normal' mgTop='-3' ></InfoText>
+                <ContactText color='whiteGreen' fontSize='12' mgTop='-5'>Meu nome:</ContactText>
+                <InfoText color='whiteGreen' children={nameValue} fontWgt='normal' mgTop='-3' ></InfoText>
 
                 <ContactText color='whiteGreen' fontSize='12' mgTop='5'>Teams:</ContactText>
-                <InfoText color='whiteGreen' children={perfil.teams} fontWgt='normal' mgTop='-3' ></InfoText>
+                <InfoText color='whiteGreen' children={teamsValue || 'Contato não informado'} fontWgt='normal' mgTop='-3' ></InfoText>
 
                 <ContactText color='whiteGreen' fontSize='12' mgTop='5'>E-mail:</ContactText>
-                <InfoText color='whiteGreen' children={perfil.email} fontWgt='normal' mgTop='-3' ></InfoText>
+                <InfoText color='whiteGreen' children={emailValue} fontWgt='normal' mgTop='-3' ></InfoText>
 
                 <ContactText color='whiteGreen' fontSize='12' mgTop='5'>Curso:</ContactText>
-                <InfoText color='whiteGreen' children={perfil.course} fontWgt='normal' mgTop='-3' ></InfoText>
+                <InfoText color='whiteGreen' children={selectedCourse} fontWgt='normal' mgTop='-3' ></InfoText>
 
             </Subcontainer>
         </Windows>
